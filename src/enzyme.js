716166f356c36ec83d6a0197d0cc41dc52522384
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2021-11-29 08:37:27 -0500

    test snapshots and Card component does exist in components

--- src/enzyme.js
+++
@@ -1,5 +0,0 @@
-import Enzyme, { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'
-configure({ adapter: new Adapter() });
-export { shallow };
-export default Enzyme;
