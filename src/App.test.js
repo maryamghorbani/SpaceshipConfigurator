3e4038bb8d34148533e7cdc5ddaf86a7bbecfbbb
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2021-11-29 08:51:25 -0500

    just commit

---
+++ src/App.test.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {shallow} from 'enzyme';
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup} from '@testing-library/react'
+
+//import components
+import SpaceshipConfigurator from "./Components/SpaceshipConfigurator";
+import SelectColor from "./Components/SelectableOptions/SelectColor";
+import SelectPower from "./Components/SelectableOptions/SelectPower";
+import WrapDrive from "./Components/SelectableOptions/WrapDrive";
+import SelectOptionPackage from "./Components/SelectableOptions/SelectOptionPackage";
+import Card from "./Components/UI/Card";
+
+afterEach(cleanup) //clean up everything after each test to avoid memory leaks
+
+describe('<SpaceshipConfigurator /> with no props', () => {
+    const container = shallow(<SpaceshipConfigurator/>);
+    it('should match the snapshot', () => {
+        expect(container.html()).toMatchSnapshot();
+    });
+
+    it('should have an title', () => {
+        expect(container.find({className: 'title'}).text()).toEqual('Spaceship Configurator');
+    });
+
+    describe('<SelectColor /> with props', () => {
+        const initialColorProps = {
+            color: "#ffffff",
+            price: 0,
+            title: "Snow",
+        };
+        const colorComponent = shallow(<SelectColor {...initialColorProps} />);
+        it('should have an title', () => {
+            expect(colorComponent.find({className: 'boxTitle'}).text()).toEqual('Select color:');
+        });
+        it('should have Card component', () => {
+            expect(colorComponent.find(<Card/>)).toBeTruthy();
+        });
+    });
+    describe('<SelectPower /> with props', () => {
+        const powerComponent = shallow(<SelectPower/>);
+        it('should have an title', () => {
+            expect(powerComponent.find({className: 'boxTitle'}).text()).toEqual('Select power:');
+        });
+        it('should have Card component', () => {
+            expect(powerComponent.find(<Card/>)).toBeTruthy();
+        });
+    });
+    describe('<WrapDrive /> with props', () => {
+        const driveComponent = shallow(<WrapDrive/>);
+        it('should have an title', () => {
+            expect(driveComponent.find({className: 'boxTitle'}).text()).toEqual('Wrap drive:');
+        });
+        it('should have Card component', () => {
+            expect(driveComponent.find(<Card/>)).toBeTruthy();
+        });
+    });
+    describe('<SelectOptionPackage /> with props', () => {
+        const packageComponent = shallow(<SelectOptionPackage/>);
+        it('should have an title', () => {
+            expect(packageComponent.find({className: 'boxTitle'}).text()).toEqual('Select option package:');
+        });
+        it('should have Card component', () => {
+            expect(packageComponent.find(<Card/>)).toBeTruthy();
+        });
+    });
+
+});
+
+
+
--- src/enzyme.js
+++ src/enzyme.js
@@ -1,5 +1,4 @@
-import Enzyme, { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 configure({ adapter: new Adapter() });
-export { shallow };
-export default Enzyme;
